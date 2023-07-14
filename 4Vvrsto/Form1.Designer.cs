847914de478efaa617aad5923f7cba334558b7fb
Gregor <gregorkuhelj3@gmail.com>
2023-07-14 19:46:08 +0200

    Add project files.

---
+++ 4Vvrsto/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace _4Vvrsto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gbZacni = new System.Windows.Forms.Button();
+            this.napisSVV = new System.Windows.Forms.Label();
+            this.gameBoardPanel = new System.Windows.Forms.Panel();
+            this.gbTezka = new System.Windows.Forms.Button();
+            this.gbLahki = new System.Windows.Forms.Button();
+            this.napisTezavnost = new System.Windows.Forms.Label();
+            this.gbReset = new System.Windows.Forms.Button();
+            this.gbRobot = new System.Windows.Forms.Button();
+            this.napisZmage = new System.Windows.Forms.Label();
+            this.zmageRdec = new System.Windows.Forms.Label();
+            this.zmageRumeni = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.gameBoardPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbZacni
+            // 
+            this.gbZacni.AutoSize = true;
+            this.gbZacni.Location = new System.Drawing.Point(0, 219);
+            this.gbZacni.Margin = new System.Windows.Forms.Padding(2);
+            this.gbZacni.Name = "gbZacni";
+            this.gbZacni.Size = new System.Drawing.Size(112, 98);
+            this.gbZacni.TabIndex = 0;
+            this.gbZacni.Text = "Ti in še en igralec";
+            this.gbZacni.UseVisualStyleBackColor = true;
+            this.gbZacni.MouseClick += new System.Windows.Forms.MouseEventHandler(this.gbZacni_Click);
+            // 
+            // napisSVV
+            // 
+            this.napisSVV.AutoSize = true;
+            this.napisSVV.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.napisSVV.ForeColor = System.Drawing.Color.Red;
+            this.napisSVV.Location = new System.Drawing.Point(128, 31);
+            this.napisSVV.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.napisSVV.Name = "napisSVV";
+            this.napisSVV.Size = new System.Drawing.Size(104, 126);
+            this.napisSVV.TabIndex = 1;
+            this.napisSVV.Text = "Štiri\r\n   v\r\nvrsto";
+            // 
+            // gameBoardPanel
+            // 
+            this.gameBoardPanel.Controls.Add(this.gbTezka);
+            this.gameBoardPanel.Controls.Add(this.gbLahki);
+            this.gameBoardPanel.Controls.Add(this.napisTezavnost);
+            this.gameBoardPanel.Controls.Add(this.gbReset);
+            this.gameBoardPanel.Controls.Add(this.napisSVV);
+            this.gameBoardPanel.Controls.Add(this.gbRobot);
+            this.gameBoardPanel.Controls.Add(this.gbZacni);
+            this.gameBoardPanel.Location = new System.Drawing.Point(140, 10);
+            this.gameBoardPanel.Margin = new System.Windows.Forms.Padding(2);
+            this.gameBoardPanel.Name = "gameBoardPanel";
+            this.gameBoardPanel.Size = new System.Drawing.Size(452, 349);
+            this.gameBoardPanel.TabIndex = 2;
+            this.gameBoardPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.Narisi);
+            this.gameBoardPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.gameBoardPanel_MouseClick);
+            // 
+            // gbTezka
+            // 
+            this.gbTezka.Location = new System.Drawing.Point(224, 245);
+            this.gbTezka.Margin = new System.Windows.Forms.Padding(2);
+            this.gbTezka.Name = "gbTezka";
+            this.gbTezka.Size = new System.Drawing.Size(86, 48);
+            this.gbTezka.TabIndex = 8;
+            this.gbTezka.Text = "Težka";
+            this.gbTezka.UseVisualStyleBackColor = true;
+            // 
+            // gbLahki
+            // 
+            this.gbLahki.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbLahki.Location = new System.Drawing.Point(27, 243);
+            this.gbLahki.Margin = new System.Windows.Forms.Padding(2);
+            this.gbLahki.Name = "gbLahki";
+            this.gbLahki.Size = new System.Drawing.Size(73, 48);
+            this.gbLahki.TabIndex = 8;
+            this.gbLahki.Text = "Lahka";
+            this.gbLahki.UseVisualStyleBackColor = true;
+            this.gbLahki.MouseClick += new System.Windows.Forms.MouseEventHandler(this.gbLahki_MouseClick);
+            // 
+            // napisTezavnost
+            // 
+            this.napisTezavnost.AutoSize = true;
+            this.napisTezavnost.Font = new System.Drawing.Font("Perpetua Titling MT", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.napisTezavnost.ForeColor = System.Drawing.Color.White;
+            this.napisTezavnost.Location = new System.Drawing.Point(2, 133);
+            this.napisTezavnost.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.napisTezavnost.Name = "napisTezavnost";
+            this.napisTezavnost.Size = new System.Drawing.Size(389, 29);
+            this.napisTezavnost.TabIndex = 7;
+            this.napisTezavnost.Text = "Izberi težavnost robota:";
+            // 
+            // gbReset
+            // 
+            this.gbReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbReset.Location = new System.Drawing.Point(380, 144);
+            this.gbReset.Margin = new System.Windows.Forms.Padding(2);
+            this.gbReset.Name = "gbReset";
+            this.gbReset.Size = new System.Drawing.Size(56, 61);
+            this.gbReset.TabIndex = 6;
+            this.gbReset.Text = "Nova igra";
+            this.gbReset.UseVisualStyleBackColor = true;
+            this.gbReset.MouseClick += new System.Windows.Forms.MouseEventHandler(this.gbReset_Click);
+            // 
+            // gbRobot
+            // 
+            this.gbRobot.AutoSize = true;
+            this.gbRobot.Location = new System.Drawing.Point(209, 219);
+            this.gbRobot.Margin = new System.Windows.Forms.Padding(2);
+            this.gbRobot.Name = "gbRobot";
+            this.gbRobot.Size = new System.Drawing.Size(112, 98);
+            this.gbRobot.TabIndex = 0;
+            this.gbRobot.Text = "Ti in robot";
+            this.gbRobot.UseVisualStyleBackColor = true;
+            this.gbRobot.MouseClick += new System.Windows.Forms.MouseEventHandler(this.gbRobot_Click);
+            // 
+            // napisZmage
+            // 
+            this.napisZmage.AutoSize = true;
+            this.napisZmage.BackColor = System.Drawing.Color.Blue;
+            this.napisZmage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.napisZmage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.napisZmage.ForeColor = System.Drawing.Color.Transparent;
+            this.napisZmage.Location = new System.Drawing.Point(-1, 66);
+            this.napisZmage.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.napisZmage.Name = "napisZmage";
+            this.napisZmage.Size = new System.Drawing.Size(147, 22);
+            this.napisZmage.TabIndex = 3;
+            this.napisZmage.Text = "ŠTEVILO ZMAG:";
+            // 
+            // zmageRdec
+            // 
+            this.zmageRdec.AutoSize = true;
+            this.zmageRdec.BackColor = System.Drawing.Color.Red;
+            this.zmageRdec.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.zmageRdec.ForeColor = System.Drawing.Color.Black;
+            this.zmageRdec.Location = new System.Drawing.Point(9, 153);
+            this.zmageRdec.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.zmageRdec.Name = "zmageRdec";
+            this.zmageRdec.Size = new System.Drawing.Size(52, 55);
+            this.zmageRdec.TabIndex = 4;
+            this.zmageRdec.Text = "0";
+            // 
+            // zmageRumeni
+            // 
+            this.zmageRumeni.AutoSize = true;
+            this.zmageRumeni.BackColor = System.Drawing.Color.Yellow;
+            this.zmageRumeni.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.zmageRumeni.ForeColor = System.Drawing.Color.Black;
+            this.zmageRumeni.Location = new System.Drawing.Point(79, 153);
+            this.zmageRumeni.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.zmageRumeni.Name = "zmageRumeni";
+            this.zmageRumeni.Size = new System.Drawing.Size(52, 55);
+            this.zmageRumeni.TabIndex = 5;
+            this.zmageRumeni.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Blue;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.zmageRumeni);
+            this.Controls.Add(this.zmageRdec);
+            this.Controls.Add(this.napisZmage);
+            this.Controls.Add(this.gameBoardPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gameBoardPanel.ResumeLayout(false);
+            this.gameBoardPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button gbZacni;
+        private System.Windows.Forms.Label napisSVV;
+        private System.Windows.Forms.Panel gameBoardPanel;
+        private System.Windows.Forms.Label napisZmage;
+        private System.Windows.Forms.Label zmageRdec;
+        private System.Windows.Forms.Label zmageRumeni;
+        private System.Windows.Forms.Button gbReset;
+        private System.Windows.Forms.Button gbRobot;
+        private System.Windows.Forms.Button gbTezka;
+        private System.Windows.Forms.Button gbLahki;
+        private System.Windows.Forms.Label napisTezavnost;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 4Vvrsto/Form1.cs
@@ -0,0 +1,407 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace _4Vvrsto
+{
+
+    // TODO: Različna režavnost robota
+    // Grafika izboljšava
+
+
+
+
+    public partial class Form1 : Form
+    {
+        int[,] plosca = new int[7, 6];
+        bool naVrstiRumeni = true;
+        bool konecIgre = false;
+        int stZmagRdeci = 0;
+        int stZmagRumeni = 0;
+        bool igraZaceta = false;
+        bool gbLahki_pritisnjen = false;
+        private int col;
+        private int row;
+        public Form1()
+        {
+            InitializeComponent();
+
+            napisZmage.Visible = false;
+            zmageRdec.Visible = false;
+            zmageRumeni.Visible = false;
+            gbReset.Visible = false;
+            gbLahki.Visible = false;
+            gbTezka.Visible = false;
+            napisTezavnost.Visible = false;
+
+            //
+            //gameBoardPanel.MouseClick += gameBoardPanel_MouseClick;
+        }
+
+
+        private void gbZacni_Click(object sender, MouseEventArgs e)
+        {
+            gbZacni.Visible = false;
+            napisSVV.Visible = false;
+            napisZmage.Visible = true;
+            gbLahki_pritisnjen = false;
+            zmageRdec.Visible = true;
+            zmageRumeni.Visible = true;
+            gbRobot.Visible = false;
+            igraZaceta = true;
+            gameBoardPanel.Invalidate(); // Trigger the Paint event to update the graphics
+                                         // gameBoardPanel.Paint += Narisi;
+        }
+
+
+        private void gbReset_Click(object sender, EventArgs e)
+        {
+            // spraznemo igralno plosco
+            plosca = new int[7, 6];
+            naVrstiRumeni = true;
+            konecIgre = false;
+            gameBoardPanel.Invalidate();  // Osvezimo gameBoardPanel, da se novi krogi gor narisejo (klice metodo Narisi
+            gbReset.Visible = false;
+
+
+
+        }
+
+        /// <summary>
+        /// Spremenimo napise ob kliku na igro z robotom
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void gbRobot_Click(object sender, MouseEventArgs e)
+        {
+
+            gbZacni.Visible = false;
+            napisSVV.Visible = false;
+            napisZmage.Visible = true;
+            zmageRdec.Visible = true;
+            zmageRumeni.Visible = true;
+            napisZmage.Visible = false;
+            zmageRdec.Visible = false;
+            zmageRumeni.Visible = false;
+            gbRobot.Visible = false;
+            gbTezka.Visible = true;
+            gbLahki.Visible = true;
+            napisTezavnost.Visible = true;
+
+
+
+
+        }
+
+
+        /// <summary>
+        /// Narise mrezo in ustrezne kroge, ki jih igralca polozita
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Narisi(object sender, PaintEventArgs e)
+        {
+            if (igraZaceta)
+            {
+                Graphics g = e.Graphics;
+
+
+                int stVrstic = plosca.GetLength(1);
+                int stStolpcev = plosca.GetLength(0);
+                int cellSize = 50;
+                int padding = 10;
+
+                int sirinaPlosce = stStolpcev * cellSize;
+                int visinaPlosce = stVrstic * cellSize;
+
+
+                // narisemo mrezo (x koordinate)
+                for (int col = 0; col <= stStolpcev; col++)
+                {
+                    int x = col * cellSize + padding;
+                    Rectangle rect = new Rectangle(x, padding, 1, visinaPlosce);
+                    g.FillRectangle(Brushes.White, rect);
+                }
+
+                // Narisemo mrezo (y koordinate)
+                for (int row = 0; row <= stVrstic; row++)
+                {
+                    int y = row * cellSize + padding;
+                    Rectangle rect = new Rectangle(padding, y, sirinaPlosce, 1);
+                    g.FillRectangle(Brushes.White, rect);
+                }
+                // Narisemo kroge, ki jih igralca polozita
+                for (int row = 0; row < stVrstic; row++)
+                {
+                    for (int col = 0; col < stStolpcev; col++)
+                    {
+                        int x = col * cellSize + padding;
+                        int y = row * cellSize + padding;
+
+                        if (plosca[col, row] == 1)
+                            g.FillEllipse(Brushes.Yellow, x, y, cellSize, cellSize);
+                        else if (plosca[col, row] == 2)
+                            g.FillEllipse(Brushes.Red, x, y, cellSize, cellSize);
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// metoda, ki se klice ob kliku na panel.
+        /// Ce je igre konec, se metoda neha izvajat.
+        /// Ce je prostor, na katerega je uporabnik pritisnil veljaven,
+        /// se preveri ali je kdo zmagal, ali je igralna plosca polna in ali je igre konec.
+        /// Ce se nic od tega ni zgodilo, se spremeni igralec, ki je na vrsti za postavitev zetona
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void gameBoardPanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            int stVrstic = plosca.GetLength(1);
+            int stStolpcev = plosca.GetLength(0);
+            int cellSize = 50;
+            int padding = 10;
+            col = (e.X - padding) / cellSize;
+            row = KjeLahkoPolozim(col);
+
+            if (!(col < 0 || col > 6))
+            {
+                if (konecIgre)
+                    return;
+
+                if (col >= 0 && col < stStolpcev && row >= 0 && row < stVrstic && plosca[col, row] == 0)
+                {
+                    if (gbLahki_pritisnjen) //  igra z robotom
+                    {
+
+                        if (naVrstiRumeni) // rumen je na vrsti
+                        {
+                            plosca[col, row] = 1;
+                        }
+                        gameBoardPanel.Invalidate(); // Sprozimo Narisi metodo, da doda zeton
+
+                        if (PreveriZmago(col, row))
+                        {
+                            // ce naVrstiRumeni vrne True, je string Rumeni, ce ne je Rdeči
+
+                            MessageBox.Show("Rumeni je zmagal!");
+                            konecIgre = true;
+                            gbReset.Visible = true;
+                            stZmagRumeni += 1;
+                            zmageRumeni.Text = $"{stZmagRumeni}";
+                            return;
+                        }
+                        GenerateRandomMove();
+
+                        gameBoardPanel.Invalidate(); // Sprozimo Narisi metodo, da doda zeton
+
+                        if (PreveriZmago(col, row))
+                        {
+                            // ce naVrstiRumeni vrne True, je string Rumeni, ce ne je Rdeči
+                            MessageBox.Show("Rdeči je zmagal!");
+                            konecIgre = true;
+                            gbReset.Visible = true;
+                            stZmagRdeci += 1;
+                            zmageRdec.Text = $"{stZmagRdeci}";
+
+                        }
+                        else if (PloscaJePolna())
+                        {
+                            MessageBox.Show("Nihče ni zmagal.");
+                            konecIgre = true;
+                        }
+                    }
+
+                    else // igra med dvema clovekoma
+                    {
+
+                        plosca[col, row] = naVrstiRumeni ? 1 : 2;
+                        gameBoardPanel.Invalidate(); // Sprozimo Narisi metodo, da doda zeton
+
+                        if (PreveriZmago(col, row))
+                        {
+                            // ce naVrstiRumeni vrne True, je string Rumeni, ce ne je Rdeči
+                            string zmagovalec = naVrstiRumeni ? "Rumeni" : "Rdeči";
+                            MessageBox.Show(zmagovalec + " je zmagal!");
+                            konecIgre = true;
+                            gbReset.Visible = true;
+                            if (naVrstiRumeni)
+                            {
+                                stZmagRumeni += 1;
+                                zmageRumeni.Text = $"{stZmagRumeni}";
+                            }
+                            else
+                            {
+                                stZmagRdeci += 1;
+                                zmageRdec.Text = $"{stZmagRdeci}";
+                            }
+                        }
+                        else if (PloscaJePolna())
+                        {
+                            MessageBox.Show("Nihče ni zmagal.");
+                            konecIgre = true;
+                        }
+                        naVrstiRumeni = !naVrstiRumeni;
+                    }
+
+                }
+            }
+        }
+
+        private void gbLahki_MouseClick(object sender, MouseEventArgs e)
+        {
+            gbLahki_pritisnjen = true;
+            gbZacni.Visible = false;
+            gbLahki.Visible = false;
+            gbTezka.Visible = false;
+            napisTezavnost.Visible = false;
+            napisSVV.Visible = false;
+            napisZmage.Visible = true;
+            naVrstiRumeni = true;
+            zmageRdec.Visible = true;
+            zmageRumeni.Visible = true;
+            gbRobot.Visible = false;
+            igraZaceta = true;
+            gameBoardPanel.Invalidate();
+
+        }
+
+        private void GenerateRandomMove()
+        {
+            
+            int stStolpcev = plosca.GetLength(0);
+            int stVrstic = plosca.GetLength(1);
+            int cellSize = 50;
+            int padding = 10;
+
+
+            // Generate random coordinates within the bounds of the game board
+            Random random = new Random();
+            col = (random.Next(0, stStolpcev) * cellSize + padding) / cellSize;
+
+            row = KjeLahkoPolozim(col);
+            if (row == -1)
+            {
+                GenerateRandomMove();
+            }
+
+            if (col >= 0 && col < stStolpcev && row >= 0 && row < stVrstic && plosca[col, row] == 0)
+            {
+                plosca[col, row] = 2;
+            }
+        }
+
+        /// <summary>
+        /// Vrne vrstico v katero lahko vrzemo kovanec v podanem stoplcu.
+        /// Če vrstice ni mozno najti, vrne -1
+        /// </summary>
+        /// <param name="col"></param>
+        /// <returns></returns>
+        private int KjeLahkoPolozim(int col) // DARJO
+        {
+            if ((col < 0 || col > 6))
+            { return -1; }
+            int StVrstic = plosca.GetLength(1);
+
+            for (int row = StVrstic - 1; row >= 0; row--)
+            {
+                if (plosca[col, row] == 0)
+                    return row;
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Metoda preveri ali je nekdo zmagal igro ali ne. (Vrne true ali false)
+        /// </summary>
+        /// <param name="stolpec"></param>
+        /// <param name="vrstica"></param>
+        /// <returns></returns>
+        private bool PreveriZmago(int stolpec, int vrstica)
+        {
+            int igralec = plosca[stolpec, vrstica];
+
+            // Preverimo zmago v smeri levo-desno
+            int st = 1;
+            st += StejPonovitve(stolpec, vrstica, -1, 0, igralec);
+            st += StejPonovitve(stolpec, vrstica, 1, 0, igralec);
+            if (st >= 4)
+                return true;
+
+            // preverimo zmago v smeri gor-dol
+            st = 1;
+            st += StejPonovitve(stolpec, vrstica, 0, -1, igralec);
+            st += StejPonovitve(stolpec, vrstica, 0, 1, igralec);
+            if (st >= 4)
+                return true;
+
+            // preverimo zmago v smeri levo_diag - desno_diag
+            st = 1;
+            st += StejPonovitve(stolpec, vrstica, -1, -1, igralec);
+            st += StejPonovitve(stolpec, vrstica, 1, 1, igralec);
+            if (st >= 4)
+                return true;
+
+            st = 1;
+            st += StejPonovitve(stolpec, vrstica, -1, 1, igralec);
+            st += StejPonovitve(stolpec, vrstica, 1, -1, igralec);
+            if (st >= 4)
+                return true;
+
+            return false;
+        }
+
+
+        /// <summary>
+        /// metoda, ki preveri stevilo ponovitev rumenih/rdecih zetonov
+        /// </summary>
+        /// <param name="stolpec">stolpec, kamor je zeton postavljen</param>
+        /// <param name="vrstica">vrstica, v katero je postavljen zeton</param>
+        /// <param name="dx">sprememba x smeri (v katero smer bomo steli ponovitve)</param>
+        /// <param name="dy">sprememba v y smeri (v katero smer bomo steli ponovitve)</param>
+        /// <param name="igralec">kateri igralec je na vrsti (rdec ali rumen)</param>
+        /// <returns></returns>
+        private int StejPonovitve(int stolpec, int vrstica, int dx, int dy, int igralec)
+        {
+            int st = 0;
+            int x = stolpec + dx;
+            int y = vrstica + dy;
+
+            while (x >= 0 && x < plosca.GetLength(0) && y >= 0 && y < plosca.GetLength(1) && plosca[x, y] == igralec)
+            {
+                st++;
+                x += dx;
+                y += dy;
+            }
+
+            return st;
+        }
+
+        /// <summary>
+        /// metoda, ki preveri ali je igralna plosca polna ali ne
+        /// </summary>
+        /// <returns></returns>
+        private bool PloscaJePolna()
+        {
+            int stVrstic = plosca.GetLength(1);
+            int stStolpcev = plosca.GetLength(0);
+
+            for (int row = 0; row < stVrstic; row++)
+            {
+                for (int col = 0; col < stStolpcev; col++)
+                {
+                    if (plosca[col, row] == 0)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+
+    }
+}
+
